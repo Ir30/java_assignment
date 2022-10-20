af4706332fb8a29c8c4f201f09956729698e1ecc
irshad007 <irshadmuhammedch8@gmail.com>
2022-10-20 16:00:40 +0530

    Nummber Triangle

---
+++ NummberTriangle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class NummberTriangle 
+{
+  static void print_triangle(int n)
+  {
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            System.out.print(j+" ");
+        }
+        System.out.println();
+    }
+  } 
+
+  public static void main(String[] args) 
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the number");
+    int num = sc.nextInt();
+
+    print_triangle(num);
+     
+  }
+}
