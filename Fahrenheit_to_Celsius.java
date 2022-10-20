f5d3ca7cce29704b61a0cdc8dd919d6b6284f957
irshad007 <irshadmuhammedch8@gmail.com>
2022-10-20 15:41:33 +0530

    Fahrenheit_to_Celsius

---
+++ Fahrenheit_to_Celsius.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Fahrenheit_to_Celsius
+{
+    static float convert_to_celcius (float t)
+    {
+        return (float) ((t-32)/1.8);
+    }
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the temprature in Fahrenheit: ");
+        float temp = sc.nextFloat();
+        System.out.println(temp+" Fahrenheit is "+convert_to_celcius(temp)+" celcius");
+
+    }
+}
